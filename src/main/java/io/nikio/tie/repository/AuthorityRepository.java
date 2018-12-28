08a9a1a32e1ddc680c8b1feaac9d760d97138bbd
Nikolas Philips <nikolas.philips@gmail.com>
2018-12-28 23:19:51 +0100

    Initial application generated by JHipster-5.7.2

---
+++ src/main/java/io/nikio/tie/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package io.nikio.tie.repository;
+
+import io.nikio.tie.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/io/nikio/tie/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package io.nikio.tie.service.dto;
