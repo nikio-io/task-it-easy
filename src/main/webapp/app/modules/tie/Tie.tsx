53e77eb9b8fd1b7aad5882d7d02d1643c4789ac9
Longomir <martin.zangger@gmail.com>
2019-01-05 00:20:16 +0100

    ....

--- src/main/webapp/app/modules/tie/Tie.tsx
+++ src/main/webapp/app/modules/tie/Tie.tsx
@@ -3,18 +3,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Row, Col, Alert } from 'reactstrap';
 
-import { IRootState } from 'app/shared/reducers';
-import { getSession } from 'app/shared/reducers/authentication';
-
-import Task, { ITask } from './Task';
+import Tasks from './Tasks';
+import { getEntities } from 'app/entities/item/item.reducer';
 
 export interface ITieProp extends StateProps, DispatchProps {}
 
 export class Tie extends React.Component<ITieProp> {
-  private tasks: ITask[] = [{ id: 0, name: 'blub' }];
-
   componentDidMount() {
-    this.props.getSession();
+    this.props.getEntities();
   }
 
   render() {
@@ -24,7 +20,8 @@ export class Tie extends React.Component<ITieProp> {
           <h2>Welcome to TIE!</h2>
 
           <h3>Tasks</h3>
-          <Task tasks={this.tasks} />
+
+          <Tasks tasks={this.props.itemList} />
         </Col>
       </Row>
     );
@@ -32,11 +29,12 @@ export class Tie extends React.Component<ITieProp> {
 }
 
 const mapStateToProps = storeState => ({
-  account: storeState.authentication.account,
-  isAuthenticated: storeState.authentication.isAuthenticated
+  itemList: storeState.item.entities
 });
 
-const mapDispatchToProps = { getSession };
+const mapDispatchToProps = {
+  getEntities
+};
 
 type StateProps = ReturnType<typeof mapStateToProps>;
 type DispatchProps = typeof mapDispatchToProps;
